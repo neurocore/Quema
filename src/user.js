3d25f513e5b8b2796140816cf172fcf61a4907b2
neurocore <nkurgin@yandex.ru>
2021-04-25 21:33:59 +0700

    Ban/unban functionality for user

--- src/user.js
+++ src/user.js
@@ -1,12 +1,34 @@
 class User
 {
-  constructor(joined = 0, played = 0, is_banned = 0, banned = 0)
-  {
-    this.is_banned = is_banned;
-    this.joined = joined;
-    this.played = played;
-    this.banned = banned;
-  }
+    constructor(ban_endtime = 0, joined_cnt = 0, played_cnt = 0, banned_cnt = 0)
+    {
+        this.ban_endtime = ban_endtime;
+        this.joined_cnt = joined_cnt;
+        this.played_cnt = played_cnt;
+        this.banned_cnt = banned_cnt;
+    }
+
+    is_banned()
+    {
+        return this.ban_endtime > Date.now();
+    }
+
+    ban_for(minutes)
+    {
+        this.ban_endtime = Date.now() + minutes * 60 * 1000;
+        this.banned_cnt++;
+    }
+
+    ban_permanent()
+    {
+        this.ban_endtime = 8640000000000000;
+        this.banned_cnt++;
+    }
+
+    unban()
+    {
+        this.ban_endtime = Date.now();
+    }
 }
 
 module.exports = User;
